665583609f1de64f01ba009b85c99b569952f47b
Ana Henriques <pbapohenriques@hotmail.com>
2022-10-23 02:03:28 +0100

    code done

--- pg50196@s7edu2.di.uminho.pt:/src/utils.c
+++ pg50196@s7edu2.di.uminho.pt:/src/utils.c
@@ -8,8 +8,8 @@
 
 Point populatePoint(Point p){
     Point point;
-    point.x = p.x;
-    point.y = p.y;
+    point.x = p.x;    // coordinate X
+    point.y = p.y;    // coordinate Y
     return point;
 }
 
@@ -34,18 +34,18 @@ void init(int N, int K, Point* points, Cluster* clusters) {
     srand(10);
     for(int p = 0; p+(num-1) < N; p+=num) {
         for(int j = 0; j < num; j++){
-            points[p+j].x = (float)rand() / RAND_MAX;
-            points[p+j].y = (float)rand() / RAND_MAX;
+            points[p+j].x = (float)rand() / RAND_MAX;       // coordinate X
+            points[p+j].y = (float)rand() / RAND_MAX;       // coordinate Y
         }
         stop = p+num;
     }
     for(int p = stop; p < N; p++) {
-        points[p].x = (float)rand() / RAND_MAX;
-        points[p].y = (float)rand() / RAND_MAX;
+        points[p].x = (float)rand() / RAND_MAX;       // coordinate X
+        points[p].y = (float)rand() / RAND_MAX;       // coordinate Y
     }
     for(int i = 0; i < K; i++) {
-        clusters[i].centroid.x = points[i].x;
-        clusters[i].centroid.y = points[i].y;
+        clusters[i].centroid.x = points[i].x; //points[i].x;
+        clusters[i].centroid.y = points[i].y; //points[i].y;
         clusters[i] = addToPoints(clusters[i], clusters[i].centroid);
         points[i].cluster = i;
     }
@@ -72,8 +72,11 @@ Point findCentroid(Cluster cluster){
         x_sum += cluster.points[i].x;
         y_sum += cluster.points[i].y;
     }
+    //printf("x_sum: %f, y_sum: %f\n", x_sum, y_sum);
     cluster.centroid.x = x_sum/cluster.number_points;
     cluster.centroid.y = y_sum/cluster.number_points;
+    //printf("centroid: (%f, %f)\n", cluster.centroid.x, cluster.centroid.y);
+    //printf("num_elem_ %d\n\n", cluster.number_points);
     return cluster.centroid;
 }
 
@@ -88,6 +91,23 @@ Cluster addToPoints(Cluster cluster, Point point){
     cluster.number_points++;
     return cluster;
 }
+
+
+/*
+Cluster removeOfPoints(int pos, Cluster cluster) {
+    int stop = 0, num = (int)((cluster.number_points/2)+1);
+    for(int i = pos; i+(num-1) < cluster.number_points; i+=num){
+        for(int j = 0; j < num; j++){
+            cluster.points[i+j] = cluster.points[i+j+1];
+        }
+        stop = i+(num-1);
+    }
+    for(int i = stop+1; i < cluster.number_points; i++){
+        cluster.points[i] = cluster.points[i+1];
+    }
+    cluster.number_points = cluster.number_points-1;
+    return cluster;
+}*/
 
 
 int closestCluster(Point p, Cluster* clusters, int K){
@@ -112,9 +132,9 @@ int addToClosestCluster(int count, int K, int N, Point* points, Cluster* cluster
         value = 0;
         for(int i = 0; i < K; i++){
             clusters[i].centroid = findCentroid(clusters[i]);
-            clusters[i].points = realloc(clusters[i].points,size*sizeof(Point));
+            //clusters[i].points = realloc(clusters[i].points,size*sizeof(Point));
             clusters[i].number_points = 0;
-            clusters[i].max_points = size;
+            //clusters[i].max_points = size;
         }
     }
     int beforeClu, allEquals = 0;
@@ -134,7 +154,13 @@ int addToClosestCluster(int count, int K, int N, Point* points, Cluster* cluster
         if(beforeClu == minCluster) allEquals++;
         points[i].cluster = minCluster;
         clusters[minCluster] = addToPoints(clusters[minCluster],points[i]);
-    }
+    }/*
+    for(int i = 0; i < K; i++){
+        for(int p = 0; p < clusters[i].number_points; p++){
+            printf("Cluster %d: (%f,%f)\n", i, clusters[i].points[p].x, clusters[i].points[p].y);
+        }
+        printf("\n");
+    }*/
     printf("allEquals %d\n\n", allEquals);
     return allEquals;
 }
