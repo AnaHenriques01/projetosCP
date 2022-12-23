61c3c1b06b67286dc3e2e1553320110dc33b790c
Ana Henriques <pbapohenriques@hotmail.com>
2022-12-23 00:56:44 +0000

    update script

--- TP3/script.ps1
+++ TP3/script.ps1
@@ -1,7 +1,7 @@
-scp -r 'include' pg50184@s7edu2.di.uminho.pt:TP2;
-scp -r 'src' pg50184@s7edu2.di.uminho.pt:TP2;
-scp -r 'Makefile' pg50184@s7edu2.di.uminho.pt:TP2;
-make clean
-make k_means
-make sbatch NTASKS=2 CP_CLUSTERS=4 THREADS=4
-cat out.txt
+scp -r 'include' pg50196@s7edu2.di.uminho.pt:TP3;
+scp -r 'src' pg50196@s7edu2.di.uminho.pt:TP3;
+scp -r 'Makefile' pg50196@s7edu2.di.uminho.pt:TP3;
+ssh pg50196@s7edu2.di.uminho.pt "cd TP3/; \
+make clean; \
+make k_means; \
+make sbatch NTASKS=2 CP_CLUSTERS=4 THREADS=4"
