42cd85e3588f38a7092214255abe572061c7dd3a
Ana Henriques <pbapohenriques@hotmail.com>
2022-12-23 00:52:22 +0000

    update script

--- TP3/script.ps1
+++ TP3/script.ps1
@@ -1,7 +1,7 @@
 scp -r 'include' pg50184@s7edu2.di.uminho.pt:TP2;
 scp -r 'src' pg50184@s7edu2.di.uminho.pt:TP2;
 scp -r 'Makefile' pg50184@s7edu2.di.uminho.pt:TP2;
-ssh pg50184@s7edu2.di.uminho.pt "cd TP2/; \
- make clean; \
- make; \
- srun --partition=cpar perf stat -r 5 -c 4 -e instructions,cycles,cache-references,cache-misses bin/k_means"
+make clean
+make k_means
+make sbatch NTASKS=2 CP_CLUSTERS=4 THREADS=4
+cat out.txt
